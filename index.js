9a86fee66872ce8d477a40f27018aa40cdb40a0f
Barbarina <barbaramarinaufsm@gmail.com>
2022-04-23 18:01:39 -0300

    feat: add get user tweets endpoint

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ app.post("/sign-up", (req, res) => {
     const {username, avatar} = req.body;
     if (!username || !avatar) return res.status(400).send("Todos os campos são obrigatórios!");
     users.unshift({username, avatar});
-    res.status(201).send("OK 👍");
+    res.status(201).send("OK");
 });
 
 app.post("/tweets", (req, res) => {
@@ -22,11 +22,19 @@ app.post("/tweets", (req, res) => {
     if (!username || !tweet) return res.status(400).send("Todos os campos são obrigatórios!");
     const user = users.find(user => user.username===username);
     tweets.unshift({username, tweet, avatar: user.avatar});
-    res.status(201).send("OK 👍");
+    res.status(201).send("OK");
 });
 
 app.get("/tweets", (_req, res) => {
     res.send(tweets.slice(0, 10));
+});
+
+app.get("/tweets/:username", (req, res) => {
+    const {username} = req.params;
+    const user = users.find(user => user.username===username);
+    if (!user) return res.status(404).send("Usuário não encontrado 👎");
+    const userTweets = tweets.filter(tweet => tweet.username===username);
+    res.send(userTweets);
 });
 
 app.listen(5000, () => console.log(chalk.bold.cyanBright("Server is running at port 5000 👍")));
